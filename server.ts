f1c01392a41b610133c9a60573a86a5a79da10bc
Sayanduary <sayanduary@gmail.com>
2025-12-09 20:11:35 +0530

    add config module and update server port handling

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import app from "./src/app.ts";
-
+import { config } from "./src/config/config.ts";
 import dotenv from "dotenv";
 dotenv.config();
 const startServer = () => {
-   const port = process.env.PORT || 3004;
+   const port = config.port || 3004;
 
    app.listen(port, () => {
       console.log(`Listening on PORT ${port} `);
