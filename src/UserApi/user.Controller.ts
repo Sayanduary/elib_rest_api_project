cd0361d9134588ab20642aed826cfa5162b2eacb
Sayanduary <sayanduary@gmail.com>
2025-12-10 23:43:49 +0530

    add JWT authentication to user creation and update configuration

--- src/UserApi/user.Controller.ts
+++ src/UserApi/user.Controller.ts
@@ -2,30 +2,50 @@ import type { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import userModel from "./user.Model.ts";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config.ts";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-   const { name, email, password } = req.body;
-   // validation
-   const error = createHttpError(400, "All Fields are required");
-   if (!name || !email || !password) {
-      return next(error);
-   }
-   //Database call
-   const user = await userModel.findOne({ email: email });
+   try {
+      const { name, email, password } = req.body;
 
-   if (user) {
-      const error = createHttpError(400, "User already exist with this email");
-      return next(error);
-   }
+      // Validation
+      if (!name || !email || !password) {
+         return next(createHttpError(400, "All fields are required"));
+      }
 
-   // password --> hashing
-   const hashedPassword = await bcrypt.hash(password, 10);
-   //Process
-   
-   // Response
-   res.json({
-      message: "User Created",
-   });
+      // Check existing user
+      const existingUser = await userModel.findOne({ email });
+      if (existingUser) {
+         return next(
+            createHttpError(400, "User already exists with this email")
+         );
+      }
+
+      // Hash password
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create new user
+      const newUser = await userModel.create({
+         name,
+         email,
+         password: hashedPassword,
+      });
+
+      // Generate JWT token
+      const token = jwt.sign(
+         { sub: newUser._id.toString() },
+         config.jwtSecret as string,
+         { expiresIn: "7d" }
+      );
+
+      // Response
+      return res.status(201).json({
+         accessToken: token,
+      });
+   } catch (err) {
+      next(err);
+   }
 };
 
 export { createUser };
