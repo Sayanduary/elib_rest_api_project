875b8b430b96e943fda388265356648c3c264b14
Sayanduary <sayanduary@gmail.com>
2025-12-11 19:29:35 +0530

    done

--- src/UserApi/user.Controller.ts
+++ src/UserApi/user.Controller.ts
@@ -1,9 +1,9 @@
-import type { Request, Response, NextFunction } from "express";
-import createHttpError from "http-errors";
-import userModel from "./user.Model.ts";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
-import { config } from "../config/config.ts";
+import bcrypt from 'bcrypt';
+import type { NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config.ts';
+import userModel from './user.Model.ts';
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
    try {
@@ -11,15 +11,13 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
       // Validation
       if (!name || !email || !password) {
-         return next(createHttpError(400, "All fields are required"));
+         return next(createHttpError(400, 'All fields are required'));
       }
 
       // Check existing user
       const existingUser = await userModel.findOne({ email });
       if (existingUser) {
-         return next(
-            createHttpError(400, "User already exists with this email")
-         );
+         return next(createHttpError(400, 'User already exists with this email'));
       }
 
       // Hash password
@@ -33,11 +31,9 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       });
 
       // Generate JWT token
-      const token = jwt.sign(
-         { sub: newUser._id.toString() },
-         config.jwtSecret as string,
-         { expiresIn: "7d" }
-      );
+      const token = jwt.sign({ sub: newUser._id.toString() }, config.jwtSecret as string, {
+         expiresIn: '7d',
+      });
 
       // Response
       return res.status(201).json({
