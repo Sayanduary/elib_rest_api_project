b02249b85ca9972e140f77b379d680c15d7cbba0
Sayanduary <sayanduary@gmail.com>
2025-12-11 19:54:13 +0530

    add prettier for code formatting in package.json and package-lock.json

--- src/UserApi/user.Controller.ts
+++ src/UserApi/user.Controller.ts
@@ -1,9 +1,10 @@
-import bcrypt from 'bcrypt';
-import type { NextFunction, Request, Response } from 'express';
-import createHttpError from 'http-errors';
-import jwt from 'jsonwebtoken';
-import { config } from '../config/config.ts';
-import userModel from './user.Model.ts';
+import bcrypt from "bcrypt";
+import type { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+import { config } from "../config/config.ts";
+import userModel from "./user.Model.ts";
+import type { User } from "./userTypes.ts";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
    try {
@@ -11,29 +12,43 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
       // Validation
       if (!name || !email || !password) {
-         return next(createHttpError(400, 'All fields are required'));
+         return next(createHttpError(400, "All fields are required"));
       }
-
       // Check existing user
-      const existingUser = await userModel.findOne({ email });
-      if (existingUser) {
-         return next(createHttpError(400, 'User already exists with this email'));
+      try {
+         const existingUser = await userModel.findOne({ email });
+         if (existingUser) {
+            return next(
+               createHttpError(400, "User already exists with this email"),
+            );
+         }
+      } catch (err) {
+         return next(createHttpError(500, "Erorr while getting user"));
       }
 
       // Hash password
       const hashedPassword = await bcrypt.hash(password, 10);
 
+      let newUser: User;
       // Create new user
-      const newUser = await userModel.create({
-         name,
-         email,
-         password: hashedPassword,
-      });
+      try {
+         newUser = await userModel.create({
+            name,
+            email,
+            password: hashedPassword,
+         });
+      } catch (err) {
+         return next(createHttpError(500, "Error while creating user"));
+      }
 
       // Generate JWT token
-      const token = jwt.sign({ sub: newUser._id.toString() }, config.jwtSecret as string, {
-         expiresIn: '7d',
-      });
+      const token = jwt.sign(
+         { sub: newUser._id.toString() },
+         config.jwtSecret as string,
+         {
+            expiresIn: "7d",
+         },
+      );
 
       // Response
       return res.status(201).json({
