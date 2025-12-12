a441387decd64d213a5c83cece96c8959fe57b98
Sayanduary <sayanduary@gmail.com>
2025-12-12 18:37:08 +0530

    add multer for file uploads in book routes and update dependencies

--- src/BookApi/book.Controller.ts
+++ src/BookApi/book.Controller.ts
@@ -2,7 +2,6 @@ import type { Request, Response, NextFunction } from "express";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
    try {
-     
    } catch (err) {
       next(err);
    }
--- src/BookApi/book.Route.ts
+++ src/BookApi/book.Route.ts
@@ -1,8 +1,23 @@
 import express from "express";
 import { createBook } from "./book.Controller.ts";
+import path from "path";
+import multer from "multer";
 
 const bookRouter = express.Router();
 
-bookRouter.post("/", createBook);
+// file store local -->
+const upload = multer({
+   dest: path.resolve(__dirname, "../../public/data/uploads"),
+   limits: { fileSize: 3e7 }, // 30 mb
+});
+
+bookRouter.post(
+   "/",
+   upload.fields([
+      { name: "coverImage,", maxCount: 1 },
+      { name: "file,", maxCount: 1 },
+   ]),
+   createBook,
+);
 
 export default bookRouter;
