1a43b940640c72317a0f3f20c300e8baa5298912
Sayanduary <sayanduary@gmail.com>
2025-12-13 20:37:06 +0530

    feat: add CORS support and update config for frontend domain

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import cors from "cors";
 import GlobalErrorHandler from "./middlewares/GlobalHandler.ts";
 import userRouter from "./UserApi/user.Router.ts";
 import type { NextFunction, Request, Response } from "express";
 import bookRouter from "./BookApi/book.Route.ts";
+import { config } from "./config/config.ts";
 
 const app = express();
+app.use(
+   cors({
+      origin: config.frontend_domain,
+   }),
+);
 app.use(express.json());
 //Routes
 // http methods ;GET , POST , PUT , DELETE
--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -8,6 +8,7 @@ const _config = {
    cloud_name: process.env.CLOUD_NAME,
    api_key: process.env.API_KEY,
    api_secret: process.env.API_SECRET,
+   frontend_domain: process.env.FRONT_END_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
