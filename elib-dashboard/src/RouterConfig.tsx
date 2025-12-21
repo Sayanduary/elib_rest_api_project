fc7847d960d697a9070ecc412693483773f4d2ee
Sayanduary <sayanduary@gmail.com>
2025-12-21 10:30:41 +0530

    feat: add dashboard layout and routing for books and home pages
    - Updated package.json to include Radix UI components for avatar, dialog, and dropdown menu.
    - Configured Router to include a dashboard layout with nested routes for home and books pages.
    - Enhanced HomePage component with a new layout and an "Add Book" button.
    - Created a new Books page component.
    - Implemented a DashboardLayout component with navigation and user menu.
    - Added Zustand store for managing authentication token.
    - Introduced UI components for avatar, badge, dropdown menu, sheet, and table.
    - Adjusted TypeScript configuration to ignore deprecations.

--- elib-dashboard/src/RouterConfig.tsx
+++ elib-dashboard/src/RouterConfig.tsx
@@ -2,15 +2,28 @@ import { createBrowserRouter } from "react-router";
 import Login from "./pages/Login";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
+import DashboardLayout from "./layouts/DashboardLayout";
+import Books from "./pages/Books";
 
 const router = createBrowserRouter([
   {
     path: "/login",
+
     element: <Login />,
   },
   {
-    path: "/",
-    element: <HomePage />,
+    path: "dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+      {
+        path: "books",
+        element: <Books />,
+      },
+    ],
   },
   {
     path: "/signup",
